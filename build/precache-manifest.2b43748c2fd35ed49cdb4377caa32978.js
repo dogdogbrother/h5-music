a1dfb4d5c0869018d337e5d763a6625ed3df35f8
tsdhr1 <gougewh@sina.com>
2019-12-20 11:21:04 +0800

    打包测试下

---
+++ build/precache-manifest.2b43748c2fd35ed49cdb4377caa32978.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f323b595d5613a374c9dfac9e5bbb00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf7916df08eb903f0971",
+    "url": "/static/css/2.9bd960b0.chunk.css"
+  },
+  {
+    "revision": "bf7916df08eb903f0971",
+    "url": "/static/js/2.3661894e.chunk.js"
+  },
+  {
+    "revision": "574db6f4cadb3cfe4753472731cd8dd1",
+    "url": "/static/js/2.3661894e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e0155c8f30633c9cc29",
+    "url": "/static/js/main.09542cac.chunk.js"
+  },
+  {
+    "revision": "874f426ffa24925d830e",
+    "url": "/static/js/runtime-main.ff9beab6.js"
+  },
+  {
+    "revision": "64444b39f93291cc186672eca04e6a7f",
+    "url": "/static/media/music.64444b39.png"
+  }
+]);
--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = 'http://localhost:3000';
+export const baseUrl = 'http://49.233.185.168:3000';
 
 // axios的实例及拦截器配置
 const axiosInstance = axios.create({
